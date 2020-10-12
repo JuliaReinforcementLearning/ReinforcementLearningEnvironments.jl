a48830dac452e20d52480f707f1edff35ec0dd30
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-10-12 17:41:13 +0530

    use RLBase alias for consistency

--- src/environments/gridworlds.jl
+++ src/environments/gridworlds.jl
@@ -1,27 +1,26 @@
-import ReinforcementLearningBase
-import .GridWorlds
 export RLBaseGridWorld
+import .GridWorlds
 
-mutable struct RLBaseGridWorld{T<:GridWorlds.AbstractGridWorld} <: ReinforcementLearningBase.AbstractEnv
+mutable struct RLBaseGridWorld{T<:GridWorlds.AbstractGridWorld} <: RLBase.AbstractEnv
     env::T
 end
 
-Base.convert(::Type{ReinforcementLearningBase.AbstractEnv}, env::GridWorlds.AbstractGridWorld) = convert(RLBaseGridWorld, env)
+Base.convert(::Type{RLBase.AbstractEnv}, env::GridWorlds.AbstractGridWorld) = convert(RLBaseGridWorld, env)
 
 Base.convert(::Type{RLBaseGridWorld}, env::GridWorlds.AbstractGridWorld) = RLBaseGridWorld(env)
 
-ReinforcementLearningBase.get_state(env::RLBaseGridWorld) = GridWorlds.get_state(env.env)
+RLBase.get_state(env::RLBaseGridWorld) = GridWorlds.get_state(env.env)
 
-ReinforcementLearningBase.get_actions(env::RLBaseGridWorld) = GridWorlds.get_actions(env.env)
+RLBase.get_actions(env::RLBaseGridWorld) = GridWorlds.get_actions(env.env)
 
-ReinforcementLearningBase.get_reward(env::RLBaseGridWorld) = GridWorlds.get_reward(env)
+RLBase.get_reward(env::RLBaseGridWorld) = GridWorlds.get_reward(env)
 
-ReinforcementLearningBase.get_terminal(env::RLBaseGridWorld) = GridWorlds.get_terminal(env.env)
+RLBase.get_terminal(env::RLBaseGridWorld) = GridWorlds.get_terminal(env.env)
 
-ReinforcementLearningBase.get_legal_actions(env::RLBaseGridWorld) = GridWorlds.get_legal_actions(env.env)
+RLBase.get_legal_actions(env::RLBaseGridWorld) = GridWorlds.get_legal_actions(env.env)
 
-ReinforcementLearningBase.get_legal_actions_mask(env::RLBaseGridWorld) = GridWorlds.get_legal_actions_mask(env.env)
+RLBase.get_legal_actions_mask(env::RLBaseGridWorld) = GridWorlds.get_legal_actions_mask(env.env)
 
-ReinforcementLearningBase.reset!(env::RLBaseGridWorld) = GridWorlds.reset!(env.env)
+RLBase.reset!(env::RLBaseGridWorld) = GridWorlds.reset!(env.env)
 
 (env::RLBaseGridWorld)(action) = env.env(action)
