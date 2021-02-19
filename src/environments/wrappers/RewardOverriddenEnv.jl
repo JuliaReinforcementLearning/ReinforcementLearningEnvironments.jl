ecf0f14641fe5caa966a9f42c86af4be85ce0d9d
Jun Tian <tianjun.cpp@gmail.com>
2021-02-19 17:34:13 +0800

    simplify the definition of environment wrappers

--- src/environments/wrappers/RewardOverriddenEnv.jl
+++ src/environments/wrappers/RewardOverriddenEnv.jl
@@ -1,6 +1,6 @@
 export RewardOverriddenEnv
 
-struct RewardOverriddenEnv{F,E<:AbstractEnv} <: AbstractEnvWrapper
+struct RewardOverriddenEnv{F,E <: AbstractEnv} <: AbstractEnvWrapper
     env::E
     f::F
 end
@@ -9,16 +9,5 @@ end
 
 RewardOverriddenEnv(f) = env -> RewardOverriddenEnv(f, env)
 
-for f in vcat(RLBase.ENV_API, RLBase.MULTI_AGENT_ENV_API)
-    if f != :reward
-        @eval RLBase.$f(x::RewardOverriddenEnv, args...; kwargs...) =
-            $f(x.env, args...; kwargs...)
-    end
-end
-
 RLBase.reward(env::RewardOverriddenEnv, args...; kwargs...) =
     env.f(reward(env.env, args...; kwargs...))
-
-RLBase.state(env::RewardOverriddenEnv, ss::RLBase.AbstractStateStyle) = state(env.env, ss)
-RLBase.state_space(env::RewardOverriddenEnv, ss::RLBase.AbstractStateStyle) =
-    state_space(env.env, ss)
