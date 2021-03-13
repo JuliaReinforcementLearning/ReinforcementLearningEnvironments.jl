28c90142440f2a21356c3c9e23424cd401e610ba
Jun Tian <tianjun.cpp@gmail.com>
2021-03-13 23:14:28 +0800

    fix 129

--- src/environments/wrappers/DefaultStateStyle.jl
+++ src/environments/wrappers/DefaultStateStyle.jl
@@ -12,3 +12,6 @@ Reset the result of `DefaultStateStyle` without changing the original behavior.
 DefaultStateStyleEnv{S}(env::E) where {S,E} = DefaultStateStyleEnv{S,E}(env)
 
 RLBase.DefaultStateStyle(::DefaultStateStyleEnv{S}) where {S} = S
+
+RLBase.state(env::DefaultStateStyleEnv{S}) where S = state(env.env, S)
+RLBase.state(env::DefaultStateStyleEnv{S}, player) where S = state(env.env, S, player)
